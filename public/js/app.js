1a7b40b8449cff1960c1bed649536c479c52b2fa
Dyana Rahhal <100005475+DyanaR@users.noreply.github.com>
2022-12-27 04:15:32 -0600

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,54 @@
+
+
+const careers = document.querySelector(".careers");
+
+let firstName = document.getElementById("firstname");
+let lastName = document.getElementById("lastname");
+let email = document.getElementById("email");
+let number = document.getElementById("number");
+let loc = document.getElementById("loc");
+let position = document.getElementById("position");
+let shift = document.getElementById("shift");
+let experience = document.getElementById("experience");
+
+
+
+careers.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  let formData = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    number: number.value,
+    loc: loc.value,
+    position: position.value,
+    shift: shift.value,
+    experience: experience.value
+  }
+
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/");
+  xhr.setRequestHeader("content-type", "application/json")
+  xhr.onload = function() {
+    console.log(xhr.responseText);
+      if(xhr.responseText == "success"){
+        document.getElementById("message").innerHTML = "<span style='color: green;'> Successfully Submitted! </span>";
+        firstName.value = "";
+        lastName.value = "";
+        email.value = "";
+        number.value = "";
+        loc.value = "";
+        position.value = "";
+        shift.value = "";
+        experience.value = "";
+      } else{
+        document.getElementById("message").innerHTML = "<span style='color: red;'> Uh oh! There was a problem, please try again. </span>";
+      }
+  }
+
+  xhr.send(JSON.stringify(formData))
+})
+
+// document.querySelectorAll("#list .item")[2].style.color = "blue"; //to modify
